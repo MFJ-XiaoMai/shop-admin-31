d905fbe22177adc07c38c071a513ad18628c72ca
xiaomai <1938665585@qq.com>
2019-06-06 12:24:13 +0800

    第一次提交，测试引入饿了么UI组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//element: 1.导入组件
+import ElementUI from "element-ui"
+
+//element: 2引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+//element: 3.注册组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
